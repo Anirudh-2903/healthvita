24b49a0f72791f47017f2b95cc6ef442f04e94e1
Anirudh <anirudh.das2903@outlook.in>
2025-03-09 23:42:13 +0530

    created an onboarding patient page and integrated appwrite into the app

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,41 @@
 import Image from "next/image";
-import {Button} from "@/components/ui/button";
+import PatientForm from "@/components/forms/PatientForm";
+import Link from "next/link";
 
 export default function Home() {
   return (
-      <div>
-        <h1 className="text-white text-3xl">HealthVita</h1>
-         <Button>Click me</Button>
+      <div className="flex h-screen max-h-screen">
+          {/* TODO :- OTP VERIFICATION / PASSKEY MODAL */}
+        <section className="remove-scrollbar container my-auto">
+            <div className="sub-container max-w-[496px]">
+                <div className="flex flex-row">
+                    <Image
+                        src="/assets/icons/logo-icon.svg"
+                        alt="patient"
+                        width={1000}
+                        height={1000}
+                        className="mb-12 h-10 w-fit"
+                    />
+                    <h1 className="text-white text-2xl font-bold text-center mt-0.5 ml-3 tracking-wide">HealthVita</h1>
+                </div>
+                <PatientForm />
+                <div className="text-14-regular mt-20 flex justify-between">
+                    <p className="justify-items-end text-dark-600 xl:text-left">
+                        © 2025 HealthVita
+                    </p>
+                    <Link href="/?admin=true" className="text-green-500">
+                        Admin
+                    </Link>
+                </div>
+            </div>
+        </section>
+          <Image
+              src="/assets/images/onboarding-img.png"
+              alt="patient"
+              width={1000}
+              height={1000}
+              className="side-img max-w-[50%]"
+          />
       </div>
   );
 }
---
+++ components/CustomFormField.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import React from 'react';
+import {FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Control} from "react-hook-form";
+import {FormFieldType} from "@/components/forms/PatientForm";
+import {Input} from "@/components/ui/input";
+import Image from "next/image";
+import PhoneInput from "react-phone-number-input";
+import 'react-phone-number-input/style.css';
+import {E164Number} from "libphonenumber-js";
+
+interface CustomProps {
+    control: Control<any>,
+    fieldType: FormFieldType,
+    name: string,
+    label?: string,
+    placeholder?: string,
+    iconSrc?: string,
+    iconAlt?: string,
+    disabled?: boolean,
+    dateFormat?: string,
+    showTimeSelect?: boolean,
+    children?: React.ReactNode,
+    renderSkeleton?: (field: any) => React.ReactNode,
+}
+
+const RenderField = ({field,props}: {field: any; props: CustomProps}) => {
+    const {fieldType,placeholder,iconSrc,iconAlt} = props;
+
+    switch (fieldType) {
+      case FormFieldType.INPUT:
+        return (
+            <div className="flex rounded-md border border-dark-500 bg-dark-400">
+                {iconSrc && (
+                    <Image
+                        src={iconSrc}
+                        alt={iconAlt || 'icon'}
+                        height={24}
+                        width={24}
+                        className="ml-2"
+                    />
+                )}
+                <FormControl>
+                    <Input
+                        placeholder={placeholder}
+                        {...field}
+                        className="shad-input border-0"
+                    />
+                </FormControl>
+            </div>
+        )
+        case FormFieldType.PHONE_INPUT:
+            return (
+                <PhoneInput
+                    defaultCountry="IN"
+                    placeholder={placeholder}
+                    international
+                    withCountryCallingCode
+                    value={field.value as E164Number | undefined}
+                    onChange={field.onChange}
+                    className="input-phone"
+                />
+            );
+      default:
+          break;
+  }
+}
+
+const CustomFormField = (props: CustomProps) => {
+    const {control,fieldType,name,label,placeholder,iconSrc,iconAlt} = props;
+
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <FormItem className="flex-1">
+                    {fieldType !== FormFieldType.CHECKBOX && label && (
+                        <FormLabel>{label}</FormLabel>
+                    )}
+                    <RenderField field={field} props={props} />
+                    <FormMessage className="shad-error" />
+                </FormItem>
+            )}
+        />
+    );
+};
+
+export default CustomFormField;
